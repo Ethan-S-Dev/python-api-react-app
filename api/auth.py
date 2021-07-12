2951809e19a2e6bec792ae635ee4424fd65a8476
Ethan Shoham <ethan380s@gmail.com>
2021-07-12 23:16:29 +0300

    test2

--- api/auth.py
+++ api/auth.py
@@ -1,21 +1,23 @@
+print("strat")
 import os
 import json
 import random
 import requests
+print("badimports")
 import jwt
-
 from base64 import b64encode
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask,Response,jsonify,render_template,templating
 
+print("dotenv")
 load_dotenv(find_dotenv())
 
-
+print("os")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-
+print("flask")
 app = Flask(__name__)
 
-
+print("route")
 @app.route("/",defaults={"path":""})
 @app.route("/<path:path>")
 def catch_all(path):
@@ -25,7 +27,7 @@ def catch_all(path):
     mimetype="application/json")
 
     return resp
-
+print("--main--")
 if __name__ == "__main__":
     app.run(debug=True)
 
