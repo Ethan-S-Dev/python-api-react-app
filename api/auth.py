c199536d73d18e6dc1163cf90b63d5f4caf51725
Ethan Shoham <ethan380s@gmail.com>
2021-07-13 00:18:37 +0300

    test4

--- api/auth.py
+++ api/auth.py
@@ -1,12 +1,30 @@
+import os
+import jwt
+import json
+from dotenv import find_dotenv,load_dotenv
 from flask import Flask ,Response
+
+
+load_dotenv(find_dotenv())
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
 
 app = Flask(__name__)
 
-app.add_url_rule("/api/auth",endpoint="index")
 
-@app.endpoint("index")
+
+
+@app.route("/api/auth")
 def index():
-    return Response({"Message":"Hey"},status=200,content_type="application/json")
+    data = json.dumps({"Message":"Hey"})
+    return Response(data,status=200,content_type="application/json")
 
+@app.route("/api/auth/secret")
+def secret():
+    data = json.dumps({"Secret":JWT_SECRET_KEY})
+    return Response(data,status=200,content_type="application/json")
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
